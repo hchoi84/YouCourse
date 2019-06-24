b72afde3ce6df8b6a61dcbfbe1406e62eec77406
JOHN WEISZ <johnweisz@JOHNs-MacBook-Pro.local>
2019-06-24 16:23:57 -0700

    added urls to video

--- apps/video_app/urls.py
+++ apps/video_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.index),
+    url(r'^(?P<course_id>\d+)/add_video_form$', views.add_video_form),
+    url(r'^(?P<course_id>\d+)/add_video_post$', views.add_video_post),
 ]
--- apps/video_app/views.py
+++ apps/video_app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render, redirect, HttpResponse
 def index(request):
 
     return render(request, "video_app/index.html")
+
+def add_video_form(request):
+
+    return render(request, "video_app/index.html")
+
+def add_video_post(request):
+
+    return render(request, "video_app/index.html")
