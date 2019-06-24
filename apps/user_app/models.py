87e22e9ee7c62cbd96992c4c298bdf7bbe0fd5a0
hchoi84@gmail.comlszYeuzF3EErZcYc6exit <hchoi84@gmail.com>
2019-06-24 15:39:49 -0700

    - updated urls for login and registration - updated views.py to handle login and registration

--- apps/user_app/models.py
+++ apps/user_app/models.py
@@ -6,7 +6,41 @@ from apps.video_app.models import *
 
 # Create your models here.
 class UserManager(models.Manager):
-    pass
+    def validate_trip_creation(self, postData):
+        errors={}
+        if len(postData['destination']) < 3:
+            errors['destination_error'] = "At least 3 characters"
+        if postData['start_date'] == "":
+            errors['start_date_error'] = "Please select a date"
+        elif datetime.strptime(postData['start_date'], '%Y-%m-%d') < datetime.today():
+            errors['start_date_error'] = "Start date must be in the future. No time traveling!"
+        if postData['end_date'] == "":
+            errors['end_date_error'] = "Please select a date"
+        elif datetime.strptime(postData['end_date'], '%Y-%m-%d') < datetime.strptime(postData['start_date'], '%Y-%m-%d'):
+            errors['end_date_error'] = "End date must be after start date. No time traveling!"
+        if len(postData['plan']) < 3:
+            errors['plan_error'] = "At least 3 characters"
+        return errors
+
+    def process_trip_creation(self, postData, userid):
+        user = User.objects.get(id=userid)
+        trip_id = self.create(
+            destination = postData['destination'],
+            start_date = postData['start_date'],
+            end_date = postData['end_date'],
+            plan = postData['plan'],
+            creator = user
+        ).id
+        return trip_id
+
+    def update_trip(self, postData, trip_id):
+        trip = self.get(id=trip_id)
+        trip.destination = postData['destination']
+        trip.start_date = postData['start_date']
+        trip.end_date = postData['end_date']
+        trip.plan = postData['plan']
+        trip.save()
+        return
 
 
 class User(models.Model):
--- apps/user_app/urls.py
+++ apps/user_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.index),
+    # url(r'^dashboard$', views.dashboard),
+    url(r'^register_user$', views.register_user),
+    url(r'^login$', views.login),
+    url(r'^logout$', views.logout),
 ]
