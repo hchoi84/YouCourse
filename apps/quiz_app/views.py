ffde3084e398e934efefbe94c5c96f3b536780d3
hchoi84@gmail.comlszYeuzF3EErZcYc6exit <hchoi84@gmail.com>
2019-06-25 15:46:52 -0700

    - implemented quiz.html - updated style.css for quiz.html

--- apps/quiz_app/views.py
+++ apps/quiz_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render, redirect, HttpResponse
+from .models import Question
+from apps.video_app.models import Video
 
 def index(request):
-    return HttpResponse("views page")
+    # video = Video.objects.get(id=1)
+    # questions = Question.objects.filter(video=video)
+    return render(request, "quiz_app/quiz.html")
+
+def test(request):
+    print(request.POST)
+    return redirect("/quiz")
