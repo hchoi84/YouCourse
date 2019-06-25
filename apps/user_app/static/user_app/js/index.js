d815fa25c16e27ae2c69e1dc6629356fd0510ed9
Mauricio J. Montenegro <mjmontenegro@gmail.com>
2019-06-25 14:03:41 -0700

    added front end email check

--- apps/user_app/static/user_app/js/index.js
+++ apps/user_app/static/user_app/js/index.js
@@ -10,7 +10,9 @@ $(document).ready(function () {
     else {
       $('#fname_result').html("");
     }
-  })
+  });
+
+
   $('#last_name').keyup(function (e) {
     if ($('#last_name').val().length < 2) {
       $('#lname_result').html("At least 2 characters");
@@ -18,16 +20,29 @@ $(document).ready(function () {
     else {
       $('#lname_result').html("");
     }
-  })
+  });
+
+
   $('#register_email').keyup(function (e) {
+    var data = $('#register_form').serialize()
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if ( !regex.test($('#register_email').val())) {
       $('#register_email_result').html("Please enter a valid email");
     }
-    else {
+    else{
       $('#register_email_result').html("");
+      $.ajax({
+        url: "/email_check",
+        method: "POST",
+        data: data
+      })
+      .done(function(res){
+          $('#register_email_result').html(res);
+      })
     }
-  })
+  });
+
+
   $('#confirm_password').keyup(function (e) {
     if ($('#password').val() != $('#confirm_password').val() ) {
       $('#password_result').html("Passwords must match");
@@ -37,13 +52,16 @@ $(document).ready(function () {
     }
   });
 
+
   $(document).on("submit", "form", function (e) {
     var error = $('#password_result').html() || $('#register_email_result').html() || $('#lname_result').html() || $('#fname_result').html();
-    if (error) {
+    if (error != "") {
       e.preventDefault();
       alert("Please fix all errors before submitting")
+      // alert(error)
       return false;
     }
-  })
+  });
+
 
 });
