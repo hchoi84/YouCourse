9ac50e47de145650e826a7d1237982da3439e660
hchoi84@gmail.comlszYeuzF3EErZcYc6exit <hchoi84@gmail.com>
2019-06-25 11:24:36 -0700

    - user_app >>> views.py, profile.html - Added TODOs - user_app >>> urls.py, views.py - changed uid to user_id

--- apps/user_app/views.py
+++ apps/user_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import User
+# TODO if the logic in profile method works, remove the below 2 imports
 from apps.video_app.models import Video
 from apps.course_app.models import Course
 from django.contrib import messages
@@ -16,7 +17,7 @@ def register_user(request):
         return redirect("/")
 
     # register user
-    request.session['uid'] = User.objects.register_user(request.POST)
+    request.session['user_id'] = User.objects.register_user(request.POST)
     return redirect("/courses")
 
 def login(request):
@@ -28,21 +29,21 @@ def login(request):
         return redirect("/")
      
     # login
-    request.session['uid'] = User.objects.get(email=request.POST['email']).id
-    # request.session['hashed_uid'] = str(User.objects.hash_id(request.session['uid']))
+    request.session['user_id'] = User.objects.get(email=request.POST['email']).id
     return redirect("/courses")
 
-def profile(request, uid):
-    # if not 'uid' in request.session:
+def profile(request, user_id):
+    # if not 'user_id' in request.session:
     #     messages.error(request, "Please log in", extra_tags="log_in")
     #     return redirect("/")
-    user = User.objects.get(id=uid)
-    completed = Video.objects.filter(users_who_completed=user)
-    created = Course.objects.filter(author=user)
+    user = User.objects.get(id=user_id)
+    # completed = Video.objects.filter(users_who_completed=user)
+    # created = Course.objects.filter(author=user)
+    # TODO does the below work instead of pulling data from other tables?
     context={
         "user": user,
-        "completed": completed,
-        "created": created,
+        "completed": user.videos_completed.all(),
+        "created": user.courses_authored.all(),
     }
     return render(request, "user_app/profile.html", context)
 
