39524c245d1846adc1081e9ba631b7e97a264c91
JOHN WEISZ <johnweisz@JOHNs-MacBook-Pro.local>
2019-06-24 18:38:03 -0700

    add video, remove video

--- apps/video_app/views.py
+++ apps/video_app/views.py
@@ -6,20 +6,24 @@ def index(request):
 
 def add_video_form(request):
 
-    return render(request, "video_app/index.html")
+    return render(request, "video_app/add.html")
 
-def add_video_post(request):
-
+def add_video_post(request, course_id):
+    if request.method == 'POST':
+        Video.objects.create_video(course_id, request.POST)
     return redirect('video')
+
+def delete_video(request, video_id):
+    if request.session['user_id'] == Video.objects.get(id=video_id).User.id:
+        Video.objects.delete_video(video_id)
+    return redirect('video')
+
+# NICE TO HAVE...
 
 def edit_video_form(request):
     
-    return render(request, "video_app/index.html")
-
-def edit_video_post(request):
-
     return redirect('video')
 
-def delete_video(request):
+def edit_video_post(request):
 
     return redirect('video')
