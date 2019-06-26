a128488dd46944b3de98968a9ddf4a37699838c1
Mauricio J. Montenegro <mjmontenegro@gmail.com>
2019-06-26 10:31:43 -0700

    added admin functionality

--- python_project/urls.py
+++ python_project/urls.py
@@ -16,6 +16,31 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
+from apps.user_app.models import User as U
+class UAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(U, UAdmin)
+
+
+from apps.video_app.models import Video
+class VideoAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Video, VideoAdmin)
+
+
+from apps.quiz_app.models import Question
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Question, QuestionAdmin)
+
+
+from apps.course_app.models import Course
+class CourseAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Course, CourseAdmin)
+
+
 urlpatterns = [
     url(r'^course/', include('apps.course_app.urls')),
     url(r'^course/', include('apps.video_app.urls')),
