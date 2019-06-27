24a56980119ba519e581fdf6aa8a051a0c7027b5
Mauricio J. Montenegro <mjmontenegro@gmail.com>
2019-06-27 13:08:26 -0700

    fixed bug on add video page

--- apps/video_app/static/video_app/js/create.js
+++ apps/video_app/static/video_app/js/create.js
@@ -3,8 +3,8 @@
 
 
 function youtube_parser(url) {
-  let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-  let match = url.match(regExp);
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  const match = url.match(regExp);
   return (match && match[7].length == 11) ? match[7] : false;
 }
 
@@ -12,18 +12,19 @@ function youtube_parser(url) {
 $(document).ready(function () {
   console.log('loaded');
   $('#url').change(function () {
-    let youtube_id = youtube_parser($('#url').val());
-
-    $.ajax({
-      url: 'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=' + youtube_id + '&key=AIzaSyC_Rr6LKYWycQH-sYikgNswVOJj4k0t9aQ',
-      method: 'GET',
-    }).done(function (res) {
-      if ($('#title').val() === '') {
-        $('#title').val(res.items[0].snippet.title);
-      }
-      if ($('#description').val() === '') {
-        $('#description').val(res.items[0].snippet.description);
-      }
-    });
+    const youtube_id = youtube_parser($('#url').val());
+    if (youtube_id) {
+      $.ajax({
+        url: `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${  youtube_id  }&key=AIzaSyC_Rr6LKYWycQH-sYikgNswVOJj4k0t9aQ`,
+        method: 'GET',
+      }).done(function (res) {
+        if ($('#title').val() === '') {
+          $('#title').val(res.items[0].snippet.title);
+        }
+        if ($('#description').val() === '') {
+          $('#description').val(res.items[0].snippet.description);
+        }
+      });
+    }
   });
 });
